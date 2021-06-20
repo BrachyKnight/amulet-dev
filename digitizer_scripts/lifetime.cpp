beb5fa47853dfca130152f73e2de991a1cd6f66e
mgirola <massimogirola1@gmail.com>
2021-06-20 03:40:41 +0200

    coded lifetime.cpp - does not work: segmentation violation

--- digitizer_scripts/lifetime.cpp
+++ digitizer_scripts/lifetime.cpp
@@ -37,81 +37,66 @@ c++ -Wall -o lifetime lifetime.cpp amulet.cc amulet.h `root-config --cflags --gl
 #include <TVirtualPad.h>
 #include <TSystem.h>
 #include <TChain.h>
-
+#include <TInterpreter.h>
 
 using ROOT::RDF::RNode;
 using ROOT::RDataFrame;
 using ROOT::VecOps::RVec;
+using std::vector;
+using std::runtime_error;
+using std::cout;
+using std::endl;
+using std::string;
 
-#define ROOT_ANALYSIS 1
+typedef struct {
+	double sqFall, sqFallErr;
+	double sqRise, sqRiseErr;
+	double sqWdt = sqFall - sqRise;
+	double sqWdtErr = TMath::Sqrt(sqFall*sqFall + sqRise*sqRise);
+} Square_Signal;
 
-using namespace std;
-typedef RVec<double> rvecdouble_t;
+typedef struct {
+	Square_Signal start;
+	Square_Signal stop;
+	double dtFall = stop.sqFall - start.sqFall;
+	double dtFallErr = TMath::Sqrt(stop.sqFallErr*stop.sqFallErr + start.sqFallErr*start.sqFallErr);
+	double dtRise = stop.sqRise - start.sqRise;
+	double dtRiseErr = TMath::Sqrt(stop.sqRiseErr*stop.sqRiseErr + start.sqRiseErr*start.sqRiseErr);
+} Decay_Event;
 
-std::pair<double,double> DeltaTandErrSameCh( rvecdouble_t v, rvecdouble_t verr ){
-	if (v.size() != 2){	
-		cout<<"size of vector is "<<v.size()<<endl;  
+Decay_Event GetSameChSquare( RVec<double> vFall, RVec<double> vFallErr, RVec<double> vRise, RVec<double> vRiseErr  ){
+	if (!(vFall.size() == 2 && vRise.size() == 2) ){ //exactly two signals in the same pulse	
+		cout<<"size of vector is "<<vRise.size()<<endl;  
 		throw std::runtime_error( "\n check filters, which signals should I choose?" ); 
 	}
-	double DeltaT = v[1]-v[0];
-	if (DeltaT < 0)
-		throw std::runtime_error( "\n DeltaT<0 does not make any sense" ); //here there must be an error because these are consecutive square waves of the same waveform
-	double DeltaT_err = sqrt(verr[1]*verr[1] + verr[0]*verr[0]);
-	return make_pair( DeltaT, DeltaT_err );
+	Square_Signal start{ vFall[0], vFallErr[0], vRise[0], vRiseErr[0] };
+	Square_Signal stop { vFall[1], vFallErr[1], vRise[1], vRiseErr[1] };
+	Decay_Event decay{ start, stop };
+	if (decay.dtFall < 0 || decay.dtRise < 0)
+		throw std::runtime_error( "\n DeltaT<0 does not make any sense" ); //consecutive signals in the same pulse
+	return decay;
 }
-std::pair<double,double> DeltaTandErrDifferentChs( rvecdouble_t v0, rvecdouble_t v0err,  rvecdouble_t v1, rvecdouble_t v1err ){
-	if (!( (v1.size()==v0.size()) == 1 )){	
-		cout<<"size of vectors are "<<v0.size()<<" and "<<v1.size()<<endl;  
+
+Decay_Event GetDiffChSquare(	RVec<double> v0Fall, RVec<double> v0FallErr,  RVec<double> v1Fall, RVec<double> v1FallErr,
+	       			RVec<double> v0Rise, RVec<double> v0RiseErr,  RVec<double> v1Rise, RVec<double> v1RiseErr ){
+	if (!( (v1Fall.size()==v0Fall.size()) && (v1Rise.size()==v0Rise.size()) && ( v1Rise.size() == 1 ) )){//exactly one signal in each pulse (one for each channel)
+		cout<<"size of vectors are "<<v0Fall.size()<<" and "<<v1Fall.size()<<endl;  
 		throw std::runtime_error( "\ncheck filters, which signals should I choose?" ); 
 	}
-	double DeltaT = v1[0]-v0[0];
-	if (DeltaT < 0)
-		cout<<"\nWARNING: DeltaT="<<DeltaT<<"<0, are columns in the right order?\n If yes you should treat these events\n"; //here it could happen? what are these signals?
-	double DeltaT_err = sqrt(v0err[0]*v0err[0] + v1err[0]*v1err[0]);
-	return make_pair( DeltaT, DeltaT_err );
+	Square_Signal start{ v1Fall[0], v1FallErr[0], v1Rise[0], v1RiseErr[0] };
+	Square_Signal stop { v0Fall[1], v0FallErr[1], v0Rise[1], v0RiseErr[1] };
+	Decay_Event decay{ start, stop };
+	if (decay.dtFall < 2*abs(decay.dtRiseErr) || decay.dtRise < 2*abs(decay.dtFallErr))
+		throw std::runtime_error( "\n DeltaT<0 does not make any sense" ); //consecutive signals in the same pulse
+	return decay;
 }
-
-std::pair<double,double> PositionMidpointStartStopSquareWaves(rvecdouble_t vUp, rvecdouble_t vDwn, rvecdouble_t v1Up, rvecdouble_t v1Dwn){
-//returns an ordered pair with the mean position btw the times of the rising and the falling edges of the (START, STOP) square waves
-	if ( vUp.size() == 2 && vDwn.size() == 2 ){
-		return make_pair((vUp[0] + vDwn[0])/2., (vUp[1] + vDwn[1])/2.);
-	}
-	else if ( vUp.size() == 1 && vDwn.size() == 1 && v1Up.size() == 1 && v1Dwn.size() == 1 ){
-		return make_pair((vUp[0] + vDwn[0])/2., (v1Up[0] + v1Dwn[0])/2.);
-	}
-	else{
-		cout<<"size of vectors are:"<<endl
-		    <<"first signal: "<<vUp.size()<<" and "<<vDwn.size()<<endl
-		    <<"second signal: "<<v1Up.size()<<" and "<<v1Dwn.size()<<endl;
-		throw std::runtime_error( "\n check filters, which signals should I choose?" );
-	}
-}
-
-std::pair<double,double> WidthStartStopSquareWaves(rvecdouble_t vUp, rvecdouble_t vDwn, rvecdouble_t v1Up, rvecdouble_t v1Dwn){ 
-//returns an ordered pair with the mean position btw the times of the rising and the falling edges of the (START, STOP) square waves
-	if ( vUp.size() == 2 && vDwn.size() == 2 ){
-		return make_pair((vUp[0] - vDwn[0]), (vUp[1] - vDwn[1]));
-	}
-	else if ( vUp.size() == 1 && vDwn.size() == 1 && v1Up.size() == 1 && v1Dwn.size() == 1 ){
-		return make_pair((vUp[0] - vDwn[0]), (v1Up[0] - v1Dwn[0]));
-	}
-	else
-	{
-		cout<<"size of vectors are:"<<endl
-		    <<"first signal: "<<vUp.size()<<" and "<<vDwn.size()<<endl
-		    <<"second signal: "<<v1Up.size()<<" and "<<v1Dwn.size()<<endl;
-		throw std::runtime_error( "\n check filters, which signals should I choose?" );
-	}
-}
-
-
 
 int main(int argc, char** argv)
 {
 	//set names from input line
 	if ( !(argc >= 4) ){ 
 		std::cout<<"\nINPUT ERROR"<<endl
-		<<"needs:\npath/to/output/directory/ \npath/to/root/input/files/directory/"
+		<<"needs:\n./path/to/executable/ path/to/output/directory/ \npath/to/root/input/files/directory/"
 		<<"RUNnumber MEASnumbers (dont specify for all)"<<endl
 		<<"examples: (for saving output into ../DAQprocessed/ and taking data from ../DAQpreproccesed/"
 		<<"./executable ../DAQprocessed/ ../DAQpreproccesed/ 1 2 //for run1meas2"<<endl
@@ -120,6 +105,10 @@ int main(int argc, char** argv)
 		<<"./executable ../DAQprocessed/ ../DAQpreprocessed/ 1 //for all run1"<<endl;
 		return 1;
 	}
+
+	//declare the struct to the CLING compiler
+	gInterpreter->Declare("typedef struct {double sqFall, sqFallErr; double sqRise, sqRiseErr; double sqWdt = sqFall - sqRise; double sqWdtErr = TMath::Sqrt(sqFall*sqFall + sqRise*sqRise);} Square_Signal;");
+	gInterpreter->Declare("typedef struct {Square_Signal start; Square_Signal stop; double dtFall = stop.sqFall - start.sqFall; double dtFallErr = TMath::Sqrt(stop.sqFallErr*stop.sqFallErr + start.sqFallErr*start.sqFallErr); double dtRise = stop.sqRise - start.sqRise; double dtRiseErr = TMath::Sqrt(stop.sqRiseErr*stop.sqRiseErr + start.sqRiseErr*start.sqRiseErr); } Decay_Event;");
 	
 	//setting I\O file names
 	string outDir = argv[1];
@@ -168,31 +157,15 @@ int main(int argc, char** argv)
 	dwnDecays.Report()->Print();
 
 	//evaluate lifetimes from digital signals defining new columns in the dataframe
-	auto dtUpDec  = upDecays.Define("DeltaTandErrSameCh"  , DeltaTandErrSameCh                                  , {"ch0timeups", "ch0timeupsErr" } )
-	                        .Define("Updt"                , [](std::pair<double,double> dt ){return dt.first;  }, {"DeltaTandErrSameCh"          } )
-	                        .Define("UpdtErr"             , [](std::pair<double,double> dt ){return dt.second; }, {"DeltaTandErrSameCh"          } )
-	                        .Define("STARTandSTOPpos"     , PositionMidpointStartStopSquareWaves                , {"ch0timeups", "ch0timedwns", "ch1timeups", "ch1timedwns"} )
-	                        .Define("UpSTARTSquareMidPos" , [](std::pair<double,double> pos){return pos.first; }, {"STARTandSTOPpos"             } )
-	                        .Define("UpSTOPSquareMidPos"  , [](std::pair<double,double> pos){return pos.second;}, {"STARTandSTOPpos"             } )
-	                        .Define("wdtStartStop"        , WidthStartStopSquareWaves                           , {"ch0timeups", "ch0timedwns", "ch1timeups", "ch1timedwns"} )
-	                        .Define("UpwdtSTART"          , [](std::pair<double,double> wdt){return wdt.first; }, {"wdtStartStop"                } )
-	                        .Define("UpwdtSTOP"           , [](std::pair<double,double> wdt){return wdt.second;}, {"wdtStartStop"                } );
+	auto dtUpDec  = upDecays.Define("decayUP"   , GetSameChSquare, {"ch0timedwns","ch0timedwnsErr","ch0timeups","ch0timeupsErr" } );
 
-	auto dtDwnDec =dwnDecays.Define("DeltaTandErrDifferentChs", DeltaTandErrDifferentChs                        , {"ch0timeups", "ch0timeupsErr", "ch1timeups", "ch1timeupsErr"} )
-	                        .Define("Dwndt"               , [](std::pair<double,double> dt){return dt.first;  } , {"DeltaTandErrDifferentChs"    } )
-	                        .Define("DwndtErr"            , [](std::pair<double,double> dt){return dt.second; } , {"DeltaTandErrDifferentChs"    } )
-	                        .Define("STARTandSTOPpos"     , PositionMidpointStartStopSquareWaves                , {"ch0timeups", "ch0timedwns", "ch1timeups", "ch1timedwns"} )
-	                        .Define("DwnSTARTSquareMidPos", [](std::pair<double,double> pos){return pos.first; }, {"STARTandSTOPpos"             } )
-	                        .Define("DwnSTOPSquareMidPos" , [](std::pair<double,double> pos){return pos.second;}, {"STARTandSTOPpos"             } )
-	                        .Define("wdtStartStop"        , WidthStartStopSquareWaves                           , {"ch0timeups", "ch0timedwns", "ch1timeups", "ch1timedwns"} )
-	                        .Define("DwnwdtSTART"         , [](std::pair<double,double> wdt){return wdt.first; }, {"wdtStartStop"                } )
-	                        .Define("DwnwdtSTOP"          , [](std::pair<double,double> wdt){return wdt.second;}, {"wdtStartStop"                } );
+	auto dtDwnDec = dwnDecays.Define("decayDOWN", GetDiffChSquare, {"ch0timeups", "ch0timeupsErr", "ch1timeups", "ch1timeupsErr","ch0timedwns", "ch0timedwnsErr", "ch1timedwns", "ch1timedwnsErr"} );
 	
 	//save dataframes in external file
-	RNode df_histUp = *dtUpDec.Snapshot("DeltaT UpDecays", OutFileName.c_str(), {"Updt", "UpdtErr", "UpSTARTSquareMidPos", "UpSTOPSquareMidPos", "UpwdtSTART", "UpwdtSTOP"} );
+	RNode df_histUp = *dtUpDec.Snapshot("DeltaT UpDecays", OutFileName.c_str(), {"measN","idx","decayUP"} );
 	ROOT::RDF::RSnapshotOptions optsDwnSnapshot;
 	optsDwnSnapshot.fMode = "UPDATE"; //to write the tree in the same file
-	RNode df_histDwn = *dtDwnDec.Snapshot("DeltaT DwnDecays", OutFileName.c_str(), {"Dwndt", "DwndtErr", "DwnSTARTSquareMidPos", "DwnSTOPSquareMidPos", "DwnwdtSTART", "DwnwdtSTOP"}, optsDwnSnapshot );
+	RNode df_histDwn = *dtDwnDec.Snapshot("DeltaT DwnDecays", OutFileName.c_str(), {"measN","idx","decayDOWN"},  optsDwnSnapshot );
 	cout<<"\nDATAFRAME CREATED IN FILE "<<OutFileName<<endl<<endl;
 	
 	return 0;
