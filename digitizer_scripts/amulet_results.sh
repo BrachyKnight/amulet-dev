b572548952a98b0fda97a72cf0ddbdc60fe76a52
mgirola <massimogirola1@gmail.com>
2021-09-29 21:15:08 +0200

    added enums in amulet.cpp and corrected run number in amulet_results.sh

---
+++ digitizer_scripts/amulet_results.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./amulet ../DAQresults/muLifetimeC.root ../DAQprocessed/run1meas_ALL.root ../DAQprocessed/run5meas_ALL.root ../DAQprocessed/run7meas_ALL.root
+./amulet ../DAQresults/muLifetimeAl.root ../DAQprocessed/run3meas_ALL.root
+./amulet ../DAQresults/muLifetimeNaCl.root ../DAQprocessed/run4meas_ALL.root
+./amulet ../DAQresults/muLifetimeBKG.root ../DAQprocessed/run6meas_ALL.root
+./amulet ../DAQresults/muLifetimeBoff.root ../DAQprocessed/run8meas_ALL.root ../DAQprocessed/run10meas_ALL.root
+./amulet ../DAQresults/muLifetimeBon.root ../DAQprocessed/run9meas_ALL.root
